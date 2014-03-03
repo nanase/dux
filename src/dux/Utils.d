65bdaf0e34cf0f06a15d6e4463e512aefa0da6a8
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-04 03:09:00 +0900

    Utils.dを追加

---
+++ src/dux/Utils.d
@@ -0,0 +1,19 @@
+﻿module dux.Utils;
+
+import std.algorithm;
+
+public:
+T clamp(T)(T min_value, T max_value, T value)
+    in
+    {
+        assert(min_value <= max_value);
+    }
+    out
+    {
+        assert(min_value <= value);
+        assert(max_value >= value);
+    }
+    body
+    {
+        return max(min_value, min(max_value, value));
+    }
