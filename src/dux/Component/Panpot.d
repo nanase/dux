3730e4773ebc4b25fa1bcedbbca7edbd3ce34e0c
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-04 03:10:32 +0900

    Panpot構造体を追加

---
+++ src/dux/Component/Panpot.d
@@ -0,0 +1,31 @@
+﻿module dux.Component.Panpot;
+
+import std.algorithm;
+import std.math;
+import dux.Utils;
+
+public struct Panpot
+{
+public:
+    immutable float l, r;
+
+public:
+    this(float lChannel, float rChannel)
+    {
+        this.l = clamp(0.0f, 1.0f, lChannel);
+        this.r = clamp(0.0f, 1.0f, rChannel);
+    }
+
+    this(float value)
+    {
+        this.l = value >= 0.0f ? cast(float)sin((value + 1f) * PI / 2.0) : 1.0f;
+        this.r = value <= 0.0f ? cast(float)sin((-value + 1f) * PI / 2.0) : 1.0f;
+    }
+
+    invariant()
+    {
+        assert(this.l >= 0.0f && this.l <= 1.0f);
+        assert(this.r >= 0.0f && this.r <= 1.0f);
+    }
+}
+
