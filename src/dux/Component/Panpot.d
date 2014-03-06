ab80bc489fb9e863f935d6dc266c795e3e9bd1b1
Tomona Nanase <nanase@users.noreply.github.com>
2014-03-06 17:00:37 +0900

    Panpotクラスに埋め込みドキュメントを追加

--- src/dux/Component/Panpot.d
+++ src/dux/Component/Panpot.d
@@ -1,25 +1,49 @@
-﻿module dux.Component.Panpot;
+﻿/** dux - ux implementation for D
+ *
+ * Authors: Tomona Nanase
+ * License: The MIT License (MIT)
+ * Copyright: Copyright (c) 2014 Tomona Nanase
+ */
+
+module dux.Component.Panpot;
 
 import std.algorithm;
 import std.math;
 import dux.Utils;
 
+/** 音の定位 (左右チャネルのバランス) を表す実数値を格納する構造体です。 */
 public struct Panpot
 {
 public:
-    immutable float l, r;
+    /** 左チャネルのレベル。 */
+    immutable float l;
+
+    /** 右チャネルのレベル。 */
+    immutable float r;
 
 public:
+    /** 左右チャネルのレベルを指定して新しい Panpot 構造体のインスタンスを初期化します。
+     * 
+     * Params:
+     *      lChannel = 左チャネルのレベル。
+     *      rChannel = 右チャネルのレベル。
+     */
     this(float lChannel, float rChannel)
     {
         this.l = clamp(0.0f, 1.0f, lChannel);
         this.r = clamp(0.0f, 1.0f, rChannel);
     }
 
+    /** 左右チャネルのレベルを制御するパンポット値を指定して新しい Panpot 構造体のインスタンスを初期化します。
+     * 
+     * Params:
+     *      value = パンポット値。
+     */
     this(float value)
         in
         {
             assert(isFinite(value));
+            assert(value >= -1.0f && value <= 1.0f);
         }
         body
         {
