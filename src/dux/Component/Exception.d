7bbda4cf00a4a685678a63a0f1329ebd62ea1e06
Tomona Nanase <nanase@users.noreply.github.com>
2014-04-04 13:34:52 +0900

    Exception.dを追加

---
+++ src/dux/Component/Exception.d
@@ -0,0 +1,24 @@
+﻿/** dux - ux implementation for D
+ *
+ * Authors: Tomona Nanase
+ * License: The MIT License (MIT)
+ * Copyright: Copyright (c) 2014 Tomona Nanase
+ */
+
+module dux.Component.Exception;
+
+import std.conv;
+
+class OutOfRangeException : Exception
+{
+    this(string file = __FILE__, int line = __LINE__)
+    {
+        super("Value is out of range.", file, line);
+    }
+
+    this(T)(string parameterName, T parameter, string message, string file = __FILE__, int line = __LINE__)
+    {
+        super(parameterName ~ " (" ~ parameter.to!string ~ ") is out of range." ~ message, file, line);
+    }
+}
+
